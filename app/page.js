795effe4675a69e009573c1e7ceedcecfc78f37a
Dhiluraj V <dhilu0001@gmail.com>
2024-03-24 09:08:54 +0530

    initial commit

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className='text-7xl'>Home Page</h1>
+      <Link href='/about' className='text-2xl'>
+        about page
+      </Link>
+    </div>
+  )
+}
+
+export default HomePage
---
+++ app/query/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ClientPage = () => {
+  return (
+    <div>
+      <h1 className='text-7xl'>Client Page</h1>
+    </div>
+  )
+}
+
+export default ClientPage
